11697c0a00fdf861f7d251f3a8c8593ea94a1a9a
Julian Rosse <julian@helixbass.net>
2021-07-25 11:32:45 -0400

    working

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,118 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {FC} from 'react';
+import {flowMax, addState, addStateHandlers, addEffect, addHandlers} from 'ad-hok'
+import {addEffectOnMount, branchIfNullish} from 'ad-hok-utils'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+interface PlayButtonProps {
+  onToggle: () => void
 }
+
+const PlayButton: FC<PlayButtonProps> = flowMax(
+  ({onToggle}) => <button onClick={onToggle}>⏯</button>
+)
+
+interface PrimaryFrequencyControlProps {
+  note: number
+  setNote: (newNote: number) => void
+}
+
+const PrimaryFrequencyControl: FC<PrimaryFrequencyControlProps> = flowMax(
+  addHandlers({
+    onChange: ({setNote}) => ({target: {value}}) => {
+      setNote(parseInt(value))
+    }
+  }),
+  ({note, onChange}) => <div>
+    Primary frequency: <input type="range" min={30} max={80} value={note} onChange={onChange} style={{width: 400}} />
+  </div>
+)
+
+interface ModulationFrequencyControlProps {
+  fmFrequency: number
+  setFmFrequency: (newFmFrequency: number) => void
+}
+
+const ModulationFrequencyControl: FC<ModulationFrequencyControlProps> = flowMax(
+  addHandlers({
+    onChange: ({setFmFrequency}) => ({target: {value}}) => {
+      setFmFrequency(parseFloat(value))
+    }
+  }),
+  ({onChange, fmFrequency}) => <div>
+    Modulation frequency: <input type="range" min={0} max={3} value={fmFrequency} step={0.05} onChange={onChange} style={{width: 400}} />
+  </div>
+)
+
+interface ModulationAmountControlProps {
+  fmAmount: number
+  setFmAmount: (newFmAmount: number) => void
+}
+
+const ModulationAmountControl: FC<ModulationAmountControlProps> = flowMax(
+  addHandlers({
+    onChange: ({setFmAmount}) => ({target: {value}}) => {
+      setFmAmount(parseFloat(value))
+    }
+  }),
+  ({onChange, fmAmount}) => <div>
+    Modulation amount: <input type="range" min={0} max={3} value={fmAmount} step={0.05} onChange={onChange} style={{width: 400}} />
+  </div>
+)
+
+type WasmImports = typeof import('wasm-local')
+
+const App: FC = flowMax(
+  addState('wasmModule', 'setWasmModule', null as WasmImports | null),
+  addEffectOnMount(({setWasmModule}) => () => {
+    import('wasm-local').then(wasmModule => {
+      setWasmModule(wasmModule)
+    })
+  }),
+  branchIfNullish('wasmModule'),
+  addStateHandlers(
+    {
+      isPlaying: false,
+    },
+    {
+      onTogglePlay: ({isPlaying}) => () => ({
+        isPlaying: !isPlaying,
+      })
+    },
+  ),
+  addState('note', 'setNote', 50),
+  addState('fmFrequency', 'setFmFrequency', 0),
+  addState('fmAmount', 'setFmAmount', 0),
+  addState('fmOsc', 'setFmOsc', null as InstanceType<WasmImports["FmOsc"]> | null),
+  addEffect(({isPlaying, setFmOsc, fmOsc, note, fmFrequency, fmAmount, wasmModule: {FmOsc}}) => () => {
+    if (isPlaying) {
+      const fmOsc = new FmOsc()
+      fmOsc.set_note(note)
+      fmOsc.set_fm_frequency(fmFrequency)
+      fmOsc.set_fm_amount(fmAmount)
+      fmOsc.set_gain(0.8)
+      setFmOsc(fmOsc)
+    } else {
+      fmOsc?.free()
+      setFmOsc(null)
+    }
+  }, ['isPlaying']),
+  addEffect(({note, fmOsc}) => () => {
+    fmOsc?.set_note(note)
+  }, ['note']),
+  addEffect(({fmFrequency, fmOsc}) => () => {
+    fmOsc?.set_fm_frequency(fmFrequency)
+  }, ['fmFrequency']),
+  addEffect(({fmAmount, fmOsc}) => () => {
+    fmOsc?.set_fm_amount(fmAmount)
+  }, ['fmAmount']),
+  ({onTogglePlay, note, setNote, fmFrequency, setFmFrequency, fmAmount, setFmAmount})  => (
+    <div>
+      <PlayButton onToggle={onTogglePlay} />
+      <PrimaryFrequencyControl note={note} setNote={setNote} />
+      <ModulationFrequencyControl fmFrequency={fmFrequency} setFmFrequency={setFmFrequency} />
+      <ModulationAmountControl fmAmount={fmAmount} setFmAmount={setFmAmount} />
+    </div>
+  )
+)
 
 export default App;
--- wasm-local/src/lib.rs
+++ wasm-local/src/lib.rs
@@ -1,6 +1,7 @@
 mod utils;
 
 use wasm_bindgen::prelude::*;
+use web_sys::{AudioContext, OscillatorType};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -9,11 +10,124 @@ use wasm_bindgen::prelude::*;
 static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
 #[wasm_bindgen]
-extern {
+extern "C" {
     fn alert(s: &str);
 }
 
 #[wasm_bindgen]
 pub fn greet() {
     alert("Hello, wasm-local!");
+}
+
+fn midi_to_freq(note: u8) -> f32 {
+    27.5 * 2f32.powf((note as f32 - 21.0) / 12.0)
+}
+
+#[wasm_bindgen]
+pub struct FmOsc {
+    ctx: AudioContext,
+    primary: web_sys::OscillatorNode,
+    gain: web_sys::GainNode,
+    fm_gain: web_sys::GainNode,
+    fm_osc: web_sys::OscillatorNode,
+    fm_freq_ratio: f32,
+    fm_gain_ratio: f32,
+}
+
+impl Drop for FmOsc {
+    fn drop(&mut self) {
+        let _ = self.ctx.close();
+    }
+}
+
+#[wasm_bindgen]
+impl FmOsc {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Result<FmOsc, JsValue> {
+        let ctx = AudioContext::new()?;
+
+        let primary = ctx.create_oscillator()?;
+        let fm_osc = ctx.create_oscillator()?;
+        let gain = ctx.create_gain()?;
+        let fm_gain = ctx.create_gain()?;
+
+        primary.set_type(OscillatorType::Sine);
+        primary.frequency().set_value(440.0);
+        gain.gain().set_value(0.0);
+        fm_gain.gain().set_value(0.0);
+        fm_osc.set_type(OscillatorType::Sine);
+        fm_osc.frequency().set_value(440.0);
+
+        primary.connect_with_audio_node(&gain)?;
+
+        gain.connect_with_audio_node(&ctx.destination())?;
+
+        fm_osc.connect_with_audio_node(&fm_gain)?;
+
+        fm_gain.connect_with_audio_param(&primary.frequency())?;
+
+        primary.start()?;
+        fm_osc.start()?;
+
+        Ok(FmOsc {
+            ctx,
+            primary,
+            gain,
+            fm_gain,
+            fm_osc,
+            fm_freq_ratio: 0.0,
+            fm_gain_ratio: 0.0,
+        })
+    }
+
+    #[wasm_bindgen]
+    pub fn set_gain(&self, mut gain: f32) {
+        if gain > 1.0 {
+            gain = 1.0;
+        }
+        if gain < 0.0 {
+            gain = 0.0;
+        }
+        self.gain.gain().set_value(gain);
+    }
+
+    #[wasm_bindgen]
+    pub fn set_primary_frequency(&self, freq: f32) {
+        self.primary.frequency().set_value(freq);
+
+        self.update_fm_osc_frequency();
+        self.update_fm_gain();
+    }
+
+    fn update_fm_osc_frequency(&self) {
+        self.fm_osc
+            .frequency()
+            .set_value(self.fm_freq_ratio * self.primary.frequency().value());
+    }
+
+    fn update_fm_gain(&self) {
+        self.fm_gain
+            .gain()
+            .set_value(self.fm_gain_ratio * self.primary.frequency().value());
+    }
+
+    #[wasm_bindgen]
+    pub fn set_note(&self, note: u8) {
+        let freq = midi_to_freq(note);
+        self.set_primary_frequency(freq);
+    }
+
+    #[wasm_bindgen]
+    pub fn set_fm_amount(&mut self, amt: f32) {
+        self.fm_gain_ratio = amt;
+
+        self.update_fm_gain();
+    }
+
+    #[wasm_bindgen]
+    pub fn set_fm_frequency(&mut self, amt: f32) {
+        self.fm_freq_ratio = amt;
+
+        self.update_fm_osc_frequency();
+    }
 }
